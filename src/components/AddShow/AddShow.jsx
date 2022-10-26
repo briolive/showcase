e7de6a66ad4ebb935f82bcf9275cc61fd94571aa
Brianna Oliveira <mystikgab@gmail.com>
2022-10-26 14:47:12 -0500

    Built form for AddShow

--- src/components/AddShow/AddShow.jsx
+++ src/components/AddShow/AddShow.jsx
@@ -4,17 +4,56 @@ import {useSelector} from 'react-redux';
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
 // component name TemplateFunction with the name for the new component.
-function TemplateFunction(props) {
+function AddShow() {
   // Using hooks we're creating local state for a "heading" variable with
   // a default value of 'Functional Component'
   const store = useSelector((store) => store);
   const [heading, setHeading] = useState('Functional Component');
 
   return (
+    <>
     <div>
-      <h2>Hello World!</h2>
+      <center>
+      <h2>Add New Show</h2>
+      </center>
     </div>
+
+    <div>
+      Artist:
+      <br />
+      <input type="text">
+      </input>
+
+      <br />
+      Supporting artist(s):
+      <br />
+      <input type="text">
+      </input>
+      
+      <br />
+      Venue:
+      <br/>
+      <input type="text">
+      </input>
+
+      <br />
+      Date:
+      <br />
+      <input type="text">
+      </input>
+
+      <br />
+      What would you like to remember about this show?
+      <br />
+      <input type="text">
+      </input>
+
+      <br />
+      <input type="submit">
+        </input>      
+    </div>
+    </>
   );
 }
 
-export default TemplateFunction;
+export default AddShow;
