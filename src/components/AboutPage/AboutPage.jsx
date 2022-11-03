9f5ca2ea77407e5271b4cbb961e51cfdbf69635c
Brianna Oliveira <mystikgab@gmail.com>
2022-11-03 17:48:48 -0500

    Changed font, updated About

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -5,6 +5,7 @@ function AboutPage() {
     <div className="container">
       <div>
         {/* <center> */}
+        <h2>About</h2>
         <h3>Technologies</h3>
         <ul>
           <li>HTML</li>
@@ -18,7 +19,7 @@ function AboutPage() {
         <h3>Next Steps</h3>
         <p>I'd like to add statistics and a map function to display the different venues each user has been to for previous concerts.</p>
         <h3>Thanks</h3>
-        <p>Thanks to my partner Emma, my friends and family, and Prime. Thank you Chris, my study group, and everyone in the Phrygian cohort.</p> 
+        <p>Thanks to my partner Emma, my friends and family, and Prime. Thanks also to my instructor Chris, my study group, and everyone in the Phrygian cohort!</p> 
         {/* </center> */}
       </div>
     </div>
